14dd814d2aa34e3bf4979401f351c96c4a33d80f
AlexZ-WP <alexz10@mail.ru>
2025-01-15 13:34:40 +0300

    Add files via upload

--- module_10_5_tr.py
+++ module_10_5_tr.py
@@ -5,26 +5,24 @@ import threading
 from multiprocessing import Pool
 
 def read_info(name):
+
     all_data =[]
     for i in name:
         with open(i, 'r', encoding='utf-8') as f: #Открывать файл name для чтения.
-            lines = f.readline()# Считывать информацию построчно (readline), пока считанная строка не окажется пустой.
-            for line in lines:
-                if line.strip():
-                    all_data.append(f.readline())  # Во время считывания добавлять каждую строку в список all_data.
-                else:
-                    return
-        
+            f.readline()# Считывать информацию построчно (readline)
+            while f.readline().strip():  # проверяем пустая строка или нет
+                all_data.append(f.readline())  # добавляем каждую строку в список all_data.
 
-
+        #print(all_data)
 start_time = time.time()
-
 filenames = [f'./file {number}.txt' for number in range(1, 5)]
 name = filenames
+#start_time = time.time()
 thread1 = threading.Thread(target=read_info(name))
 thread1.start()
 end_time = time.time()
 elapsed_time = start_time - end_time
+
 print(elapsed_time)
 
 if __name__ == '__main__':
@@ -39,5 +37,5 @@ if __name__ == '__main__':
     end_time = time.time()
     elapsed_time = start_time - end_time
 
-
+#print(elapsed_time)
     print(elapsed_time)
